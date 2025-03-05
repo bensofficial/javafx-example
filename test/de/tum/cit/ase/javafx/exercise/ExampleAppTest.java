937666278244e05d63012c16a7aebd84cebc6844
Benjamin Schmitz <benjamin.schmitz@tum.de>
2025-03-05 12:45:42 +0100

    Simplify example

--- test/de/tum/cit/ase/javafx/exercise/ExampleAppTest.java
+++ test/de/tum/cit/ase/javafx/exercise/ExampleAppTest.java
@@ -2,19 +2,21 @@ package de.tum.cit.ase.javafx.exercise;
 
 import de.tum.cit.ase.eos.JavaFXTest;
 import de.tum.cit.ase.eos.TestFXMacAnnotations;
+import de.tum.in.test.api.MirrorOutput;
+import de.tum.in.test.api.StrictTimeout;
+import de.tum.in.test.api.WhitelistClass;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 import static org.testfx.api.FxAssert.verifyThat;
-
 import static org.testfx.matcher.control.LabeledMatchers.hasText;
 
-@CustomAnnotations
+@WhitelistClass(ExampleAppTest.class)
+@StrictTimeout(10)
+@MirrorOutput
 @TestFXMacAnnotations
 public class ExampleAppTest extends JavaFXTest {
 	private Button button;
@@ -28,55 +30,17 @@ public class ExampleAppTest extends JavaFXTest {
 
 	@BeforeEach
 	public void initializeObjects() {
-		var buttonOptional = lookup(".button").tryQueryAs(Button.class);
-		if (buttonOptional.isEmpty()) {
-			fail("Button not found");
-		}
-		button = buttonOptional.get();
-
-		var textFieldOptional = lookup(".text-field").tryQueryAs(TextField.class);
-		if (textFieldOptional.isEmpty()) {
-			fail("TextField not found");
-		}
-		textField = textFieldOptional.get();
-
-		var labelOptional = lookup(".label").tryQueryAs(Label.class);
-		if (labelOptional.isEmpty()) {
-			fail("Label not found");
-		}
-		label = labelOptional.get();
-	}
-
-	@Test
-	public void testNoInput() {
-		assertEquals("Enter text here", textField.getPromptText());
-
-		verifyThat(button, hasText("Count Characters"));
-		verifyThat(label, hasText("Character count: 0"));
-
-		clickOn(button);
-
-		verifyThat(label, hasText("Character count: 0"));
+		button = getNodeOfType(Button.class, ".button");
+		textField = getNodeOfType(TextField.class, ".text-field");
+		label = getNodeOfType(Label.class, ".label");
 	}
 
 	@Test
 	public void testInput() {
-		String input = "Hello, World!";
+		String input = "Hello World";
 
 		clickOn(textField).write(input);
 		clickOn(button);
 		verifyThat(label, hasText("Character count: " + input.length()));
-
-        clickOn(textField).eraseText(input.length());
-
-        clickOn(textField).write("a");
-        clickOn(button);
-        verifyThat(label, hasText("Character count: 1"));
-
-		clickOn(textField).eraseText(1);
-
-		clickOn(textField).write("");
-		clickOn(button);
-		verifyThat(label, hasText("Character count: 0"));
 	}
 }
